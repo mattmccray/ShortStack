8d6850d0973867bd4cf2b716276b92c229fc65ab
M@ McCray <darthapo@gmail.com>
2009-12-24 20:55:52 -0600

    Added missing finder class

---
+++ source/shortstack/finder.php
@@ -0,0 +1,157 @@
+<?php
+
+class CoreFinder implements IteratorAggregate {
+  
+  protected $objtype;
+  protected $matcher = false;
+  protected $finder = array();
+  protected $or_finder = array();
+  protected $order = array();
+  protected $limit = false;
+  private $__cache = false;
+  
+  public function __construct($objtype) {
+    $this->objtype = $objtype;
+  }
+  
+  public function where($index) {
+    $this->__cache = false;
+    if(! $this->matcher) $this->matcher = new FinderMatch($this, $index);
+    $this->matcher->_updateIdxAndCls($index, 'and');
+    return $this->matcher;
+  }
+  
+  public function andWhere($index) {
+    $this->__cache = false;
+    if(! $this->matcher) $this->matcher = new FinderMatch($this, $index);
+    $this->matcher->_updateIdxAndCls($index, 'and');
+    return $this->matcher;
+  }
+
+  public function orWhere($index) {
+    $this->__cache = false;
+    if(! $this-matcher) $this->matcher = new FinderMatch($this, $index);
+    $this->matcher->_updateIdxAndCls($index, 'or');
+    return $this->matcher;
+  }
+  
+  public function order($field, $dir='ASC') {
+    $this->__cache = false;
+    $this->order[$field] = $dir;
+    return $this;
+  }
+  
+  public function limit($count) {
+    $this->__cache = false;
+    $this->limit = $count;
+    return $this;
+  }
+  
+  public function count() {
+    return count($this->fetch());
+  }
+  
+  public function get() {   // Returns the first match
+    $oldLimit = $this->limit;
+    $this->limit = 1; // Waste not, want not.
+    $docs = $this->_execQuery();
+    $this->limit = $oldLimit;
+    return @$docs[0];
+  }
+  
+  public function fetch() { // Executes current query
+    return $this->_execQuery();
+  }
+  
+  public function getIterator() { // For using the finder as an array in foreach() statements
+    $docs = $this->_execQuery();
+    return new ArrayIterator($docs);
+  }
+  
+// Warning these modified the matched records!!
+
+  public function destroy() {
+    foreach ($this as $doc) {
+      $doc->destroy();
+    }
+    $this->__cache = false;
+  }
+
+  public function update($values=array()) {
+    foreach ($this as $doc) {
+      $doc->update($values);
+      $doc->save();
+    }
+    $this->__cache = false;
+  }
+  
+  public function _addFilter($column, $comparision, $value, $clause) {
+    $this->__cache = false;
+    $finder_filter = array('col'=>$column, 'comp'=>$comparision, 'val'=>$value);
+    if($clause == 'or') $this->or_finder[] = $finder_filter;
+    else $this->finder[] = $finder_filter;
+    return $this;
+  }
+  
+  
+  protected function _execQuery() {
+    if($this->__cache != false) return $this->__cache;
+    $sql = $this->_buildSQL();
+    $stmt = DB::query($sql);
+    $items = array();
+    if($stmt) {
+      $results = $stmt->fetchAll();
+      $className = $this->objtype;
+      foreach ($results as $rowdata) {
+        $items[] = new $className($rowdata);
+      }
+      $this->__cache = $items;
+    } // FIXME: ELSE THROW SQL ERROR??? Hmm...
+    return $items;
+  }
+  
+  protected function _buildSQL() {
+    throw new Exception("_buildSQL() has not been implemented!");
+  }
+}
+
+class FinderMatch {
+  protected $finder;
+  protected $index;
+  protected $clause;
+  
+  public function __construct($finder) {
+    $this->finder = $finder;
+  }
+  
+  public function _updateIdxAndCls($idx, $clause) {
+    $this->index = $idx;
+    $this->clause = $clause;
+    return $this;
+  }
+  
+  public function eq($value) {
+    return $this->finder->_addFilter($this->index, '=', $value, $this->clause);
+  }
+  public function neq($value) {
+    return  $this->finder->_addFilter($this->index, '!=', $value, $this->clause);
+  }
+  public function gt($value) {
+    return $this->finder->_addFilter($this->index, '>', $value, $this->clause);
+  }
+  public function lt($value) {
+    return $this->finder->_addFilter($this->index, '<', $value, $this->clause);
+  }
+  public function gte($value) {
+    return $this->finder->_addFilter($this->index, '>=', $value, $this->clause);
+  }
+  public function lte($value) {
+    return $this->finder->_addFilter($this->index, '<=', $value, $this->clause);
+  }
+  public function like($value) {
+    return $this->finder->_addFilter($this->index, 'like', $value, $this->clause);
+  }
+  public function in($value) {
+    return $this->finder->_addFilter($this->index, 'in', $value, $this->clause);
+  }
+}
