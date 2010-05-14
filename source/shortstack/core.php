bb2863bee83885fccdb91474fbdf415dff40fb7f
M@ McCray <darthapo@gmail.com>
2010-05-14 17:02:39 -0500

    Updated autoloader a bit

--- source/shortstack/core.php
+++ source/shortstack/core.php
@@ -1,6 +1,10 @@
 <?php
 
 function __autoload($className) {
+  if(stripos($className, '.') >= 0) {
+    $path_parts = explode('.', $className);
+    $className = $path_parts[0]; 
+  }
   $classPath = ShortStack::AutoLoadFinder($className);
   if(!file_exists($classPath)) {
     return eval("class {$className}{public function __construct(){throw new NotFoundException('Class not found!');}}");
